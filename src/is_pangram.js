3f2b7e6dff5bfdf00d3fd36f8a1367417434bdb6
SuelyBarreto <suely@barreto.us>
2020-04-03 09:09:56 -0700

    js testing is palindrome

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -3,3 +3,25 @@ const isPangram = function(text) {
 };
 
 module.exports = isPangram;
+
+
+
+/*
+Testing
+1. Success-case input for Pangram - 
+Two driven jocks help fax my big quiz.
+Pack my box with five dozen liquor jugs
+The five boxing wizards jump quickly
+Bright vixens jump; dozy fowl quack
+Jackdaws love my big sphinx of quartz
+John quickly extemporized five tow bags
+Waltz, nymph, for quick jigs vex Bud
+Quick wafting zephyrs vex bold Jim
+
+2. Nominal failure, a clear failure case (i.e, a string with only 10 letters)
+
+3. An edge-case failure (i.e, a string with all the letters but one)
+
+4. Another edge-case failure
+
+*/
